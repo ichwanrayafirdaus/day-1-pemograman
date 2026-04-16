667ccbf05fa9db7a9d0b6db310a109ec73c5ff88
iwan <iwanrayafirdaus@gmail.com>
2026-04-16 23:33:56 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,75 +1,24 @@
-const CACHE_NAME = "toko-kita-v1";
-const BASE_URL = self.registration.scope;
+const CACHE_NAME = "toko-kita-v3";
 
 const urlsToCache = [
-  `${BASE_URL}`,
-  `${BASE_URL}index.html`,
-  `${BASE_URL}offline.html`,
-  `${BASE_URL}assets/style.css`,
-  `${BASE_URL}manifest.json`,
-  `${BASE_URL}icons/icon-192x192.png`,
-  `${BASE_URL}icons/icon-512x512.png`,
-  `${BASE_URL}icons/maskable_icon.png`,
-  `${BASE_URL}icons/logo.png`,
-  `${BASE_URL}icons/favicon.png`
+  "./",
+  "./index.html",
+  "./example.html",
+  "./manifest.json",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png"
 ];
 
-// INSTALL
 self.addEventListener("install", event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-      .catch(err => console.log("Cache gagal:", err))
   );
 });
 
-// ACTIVATE
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    (async () => {
-      const keys = await caches.keys();
-      await Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-      await self.clients.claim();
-    })()
-  );
-});
-
-// FETCH
 self.addEventListener("fetch", event => {
-  const request = event.request;
-  const url = new URL(request.url);
-
-  if (request.method !== "GET") return;
-
-  // FILE LOKAL
-  if (url.origin === self.location.origin) {
-    event.respondWith(
-      caches.match(request).then(response => {
-        return (
-          response ||
-          fetch(request).catch(() => caches.match(`${BASE_URL}offline.html`))
-        );
-      })
-    );
-  }
-
-  // FILE EKSTERNAL
-  else {
-    event.respondWith(
-      fetch(request)
-        .then(res => {
-          const clone = res.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(request, clone));
-          return res;
-        })
-        .catch(() => caches.match(request))
-    );
-  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
